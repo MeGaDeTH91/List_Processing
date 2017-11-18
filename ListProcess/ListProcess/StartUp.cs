d920f78e7918ca8573eecc9c595c39a22d8fb59d
MeGaDeTH91 <megadeth_91@abv.bg>
2017-11-18 20:03:19 +0200

    Creating Application

---
+++ ListProcess/ListProcess/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListProcess
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] initial = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+
+            Console.WriteLine($"{string.Join(" ", initial)}");
+        }
+    }
+}
