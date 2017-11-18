1bfd7cf5f142591cafed85ab1bbf73d798cc5b75
MeGaDeTH91 <megadeth_91@abv.bg>
2017-11-18 20:47:01 +0200

    Tasks 4,9 and 11

--- ListProcess/ListProcess/StartUp.cs
+++ ListProcess/ListProcess/StartUp.cs
@@ -8,12 +8,74 @@ namespace ListProcess
     {
         static void Main(string[] args)
         {
-            string[] initial = Console.ReadLine()
+            List<string> initial = Console.ReadLine()
                 .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-                .ToArray();
+                .ToList();
 
+            string input;
 
-            Console.WriteLine($"{string.Join(" ", initial)}");
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] commArgs = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string command = commArgs[0];
+
+                switch (command)
+                {
+                    case "end":
+                        if(commArgs.Length > 1)
+                        {
+                            Console.WriteLine("Error: invalid command parameters");
+                        }
+                        break;
+
+                    case "reverse":
+                        if(commArgs.Length == 1)
+                        {
+                            initial.Reverse();
+                            Console.WriteLine($"{string.Join(" ", initial)}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: invalid command parameters");
+                        }
+                        break;
+
+                    case "sort":
+                        
+                        if (commArgs.Length == 1)
+                        {
+                            initial.Sort();
+                            Console.WriteLine($"{string.Join(" ", initial)}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: invalid command parameters");
+                        }
+                        break;
+
+                    case "count":
+                        if (commArgs.Length == 2)
+                        {
+                            string countString = commArgs[1];
+
+                            int count = initial.Where(x => x == countString).Count();
+                            Console.WriteLine(count);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: invalid command parameters");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Error: invalid command");
+                        break;
+                }
+
+            }
+            Console.WriteLine("Finished");
         }
     }
 }
