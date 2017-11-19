bb51dde385332cfdb643ab82e07d0a985c185b47
killemil <esend3@yahoo.com>
2017-11-19 20:31:21 +0200

    more functionality

--- ListProcess/ListProcess/StartUp.cs
+++ ListProcess/ListProcess/StartUp.cs
@@ -1,107 +1,115 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace ListProcess
+﻿namespace ListProcess
 {
-    class StartUp
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
     {
-        static void Main(string[] args)
+        private const string InvalidCommandParamters = "Error: invalid command parameters";
+        private const string InvalidCommand = "Error: invalid command";
+        private const string InvalidIndex = "Error: Invalid index {0}";
+
+        public static void Main()
         {
             List<string> initial = Console.ReadLine()
                 .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
                 .ToList();
+            Console.WriteLine(string.Join(" ", initial));
 
             string input;
-
             while ((input = Console.ReadLine()) != "end")
             {
-                string[] commArgs = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                string[] commArgs = input
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
                     .ToArray();
-
                 string command = commArgs[0];
+
+                if (command == "END")
+                {
+                    Console.WriteLine(InvalidCommand);
+                    continue;
+                }
 
                 switch (command.ToLower())
                 {
                     case "append":
-                        if (commArgs.Length==2)
+                        if (commArgs.Length == 2)
                         {
                             string stringToAppend = commArgs[1];
 
                             initial.Add(stringToAppend);
-                            Console.WriteLine(string.Join(" ",initial));
+                            Console.WriteLine(string.Join(" ", initial));
                         }
                         else
                         {
-                            Console.WriteLine("Error: invalid command parameters");
+                            Console.WriteLine(InvalidCommandParamters);
                         }
-                        break;
 
+                        break;
                     case "prepend":
                         if (commArgs.Length == 2)
                         {
                             string stringToPrepend = commArgs[1];
 
                             initial.Insert(0, stringToPrepend);
-                            Console.WriteLine(string.Join(" ",initial));
+                            Console.WriteLine(string.Join(" ", initial));
                         }
                         else
                         {
-                            Console.WriteLine("Error: invalid command parameters");
+                            Console.WriteLine(InvalidCommandParamters);
                         }
-                        break;
 
+                        break;
                     case "roll":
-                        if (commArgs.Length==2)
+                        if (commArgs.Length == 2)
                         {
-                            if (commArgs[1]=="left")
+                            if (commArgs[1] == "left")
                             {
                                 string temp = initial.First();
                                 initial.RemoveAt(0);
                                 initial.Add(temp);
-                                Console.WriteLine(string.Join(" ",initial));
+                                Console.WriteLine(string.Join(" ", initial));
                             }
-                            else if (commArgs[1]=="right")
+                            else if (commArgs[1] == "right")
                             {
                                 string temp = initial.Last();
-                                initial.RemoveAt(initial.Count-1);
-                                initial.Insert(0,temp) ;
+                                initial.RemoveAt(initial.Count - 1);
+                                initial.Insert(0, temp);
                                 Console.WriteLine(string.Join(" ", initial));
                             }
                             else
                             {
-                                Console.WriteLine("Error: invalid command");
+                                Console.WriteLine(InvalidCommandParamters);
                             }
                         }
-
                         else
                         {
-                            Console.WriteLine("Error: invalid command parameters");
+                            Console.WriteLine(InvalidCommandParamters);
                         }
 
                         break;
-
                     case "end":
-                        if(commArgs.Length > 1)
+                        if (commArgs.Length > 1)
                         {
-                            Console.WriteLine("Error: invalid command parameters");
+                            Console.WriteLine(InvalidCommandParamters);
                         }
+
                         break;
 
                     case "reverse":
-                        if(commArgs.Length == 1)
+                        if (commArgs.Length == 1)
                         {
                             initial.Reverse();
                             Console.WriteLine($"{string.Join(" ", initial)}");
                         }
                         else
                         {
-                            Console.WriteLine("Error: invalid command parameters");
+                            Console.WriteLine(InvalidCommandParamters);
                         }
-                        break;
 
+                        break;
                     case "sort":
-                        
                         if (commArgs.Length == 1)
                         {
                             initial.Sort();
@@ -109,10 +117,10 @@ namespace ListProcess
                         }
                         else
                         {
-                            Console.WriteLine("Error: invalid command parameters");
+                            Console.WriteLine(InvalidCommandParamters);
                         }
-                        break;
 
+                        break;
                     case "count":
                         if (commArgs.Length == 2)
                         {
@@ -123,12 +131,64 @@ namespace ListProcess
                         }
                         else
                         {
-                            Console.WriteLine("Error: invalid command parameters");
+                            Console.WriteLine(InvalidCommandParamters);
                         }
-                        break;
 
+                        break;
+                    case "delete":
+                        if (commArgs.Count() != 2)
+                        {
+                            Console.WriteLine(InvalidCommandParamters);
+                        }
+                        else
+                        {
+                            var isNumber = int.TryParse(commArgs[1], out int index);
+                            if (!isNumber)
+                            {
+                                Console.WriteLine(InvalidCommandParamters);
+                            }
+                            else if (index >= initial.Count || index < 0)
+                            {
+                                Console.WriteLine(InvalidIndex, index);
+                                break;
+                            }
+                            else
+                            {
+                                initial.RemoveAt(index);
+                                Console.WriteLine(string.Join(" ", initial));
+                            }
+                        }
+
+                        break;
+                    case "insert":
+                        if (commArgs.Count() != 3)
+                        {
+                            Console.WriteLine(InvalidCommandParamters);
+                        }
+                        else
+                        {
+                            var isNumber = int.TryParse(commArgs[1], out int index);
+                            if (!isNumber)
+                            {
+                                Console.WriteLine(InvalidCommandParamters);
+                                break;
+                            }
+
+                            var value = commArgs[2];
+                            if (index >= initial.Count || index < 0)
+                            {
+                                Console.WriteLine(InvalidIndex, index);
+                            }
+                            else
+                            {
+                                initial.Insert(index, value);
+                                Console.WriteLine(string.Join(" ", initial));
+                            }
+                        }
+
+                        break;
                     default:
-                        Console.WriteLine("Error: invalid command");
+                        Console.WriteLine(InvalidCommand);
                         break;
                 }
             }
